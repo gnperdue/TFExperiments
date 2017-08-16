3c0dfb5b02d395550d094b5b0e54fed0e9a57eaa
Gabriel Perdue <gnperdue@me.com>
2017-08-16 10:14:38 -0500

    try doing the import not inside of a new graph

--- logreg/utils_mnist.py
+++ logreg/utils_mnist.py
@@ -75,9 +75,7 @@ def load_frozen_graph(graph_filename):
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
 
-    with tf.Graph().as_default() as graph:
-        tf.import_graph_def(
-            graph_def, input_map=None, return_elements=None, name='',
-            op_dict=None, producer_op_list=None
-        )
-    return graph
+    tf.import_graph_def(
+        graph_def, input_map=None, return_elements=None, name='',
+        op_dict=None, producer_op_list=None
+    )
