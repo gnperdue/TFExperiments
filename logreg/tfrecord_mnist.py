aadb10123cfd0002cf00abc172a37ee976996832
Gabriel Perdue <gnperdue@me.com>
2017-08-16 10:15:11 -0500

    add some name scoping stuff

--- logreg/tfrecord_mnist.py
+++ logreg/tfrecord_mnist.py
@@ -12,45 +12,51 @@ BATCH_SIZE = 20
 
 
 def tfrecord_to_graph_ops(filenames_list, num_epochs=1):
-    file_queue = tf.train.string_input_producer(
-        filenames_list, name='file_queue', num_epochs=num_epochs
-    )
-    reader = tf.TFRecordReader(
-        options=tf.python_io.TFRecordOptions(
-            compression_type=tf.python_io.TFRecordCompressionType.GZIP
+    with tf.variable_scope('tfrec_to_graph'):
+        file_queue = tf.train.string_input_producer(
+            filenames_list, name='file_queue', num_epochs=num_epochs
         )
-    )
-    _, tfrecord = reader.read(file_queue)
+        reader = tf.TFRecordReader(
+            options=tf.python_io.TFRecordOptions(
+                compression_type=tf.python_io.TFRecordCompressionType.GZIP
+            ), name='tfrec_reader'
+        )
+        _, tfrecord = reader.read(file_queue)
 
-    tfrecord_features = tf.parse_single_example(
-        tfrecord,
-        features={
-            'features': tf.FixedLenFeature([], tf.string),
-            'targets': tf.FixedLenFeature([], tf.string)
-        },
-        name='data'
-    )
-    features = tf.decode_raw(tfrecord_features['features'], tf.uint8)
-    features = tf.reshape(features, [-1, 784])
-    features = tf.cast(features, tf.float32)
-    targets = tf.decode_raw(tfrecord_features['targets'], tf.uint8)
-    targets = tf.cast(targets, tf.int32)
-    targets = tf.reshape(targets, [-1])
-    targets = tf.one_hot(indices=targets, depth=10, on_value=1, off_value=0)
-    return features, targets
+        tfrecord_features = tf.parse_single_example(
+            tfrecord,
+            features={
+                'features': tf.FixedLenFeature([], tf.string),
+                'targets': tf.FixedLenFeature([], tf.string)
+            },
+            name='data'
+        )
+        with tf.variable_scope('features'):
+            features = tf.decode_raw(tfrecord_features['features'], tf.uint8)
+            features = tf.reshape(features, [-1, 784])
+            features = tf.cast(features, tf.float32)
+        with tf.variable_scope('targets'):
+            targets = tf.decode_raw(tfrecord_features['targets'], tf.uint8)
+            targets = tf.cast(targets, tf.int32)
+            targets = tf.reshape(targets, [-1])
+            targets = tf.one_hot(
+                indices=targets, depth=10, on_value=1, off_value=0
+            )
+        return features, targets
 
 
 def batch_generator(filenames_list, batch_size=BATCH_SIZE, num_epochs=1):
-    features, targets = tfrecord_to_graph_ops(filenames_list, num_epochs)
-    min_after_dequeue = 3 * batch_size
-    capacity = 20 * batch_size
-    features_batch, targets_batch = tf.train.shuffle_batch(
-        [features, targets],
-        batch_size=batch_size,
-        capacity=capacity,
-        min_after_dequeue=min_after_dequeue,
-        enqueue_many=True
-    )
+    with tf.variable_scope('batchgen'):
+        features, targets = tfrecord_to_graph_ops(filenames_list, num_epochs)
+        min_after_dequeue = 3 * batch_size
+        capacity = 20 * batch_size
+        features_batch, targets_batch = tf.train.shuffle_batch(
+            [features, targets],
+            batch_size=batch_size,
+            capacity=capacity,
+            min_after_dequeue=min_after_dequeue,
+            enqueue_many=True
+        )
     return features_batch, targets_batch
 
 
@@ -80,7 +86,7 @@ def examine_batches(features_batch, targets_batch):
 
 
 def test():
-    test_file = DATA_PATH + 'mnist_test.tfrecord'
+    test_file = DATA_PATH + 'mnist_test.tfrecord.gz'
     if os.path.isfile(test_file):
         file_list = [test_file]
     else:
