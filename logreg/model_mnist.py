a44d3ad5c1e05bc4ef992c2d97c8151375449224
Gabriel Perdue <gnperdue@me.com>
2017-08-22 21:11:29 -0500

    fix summary plots and start tracking regularization loss

--- logreg/model_mnist.py
+++ logreg/model_mnist.py
@@ -7,16 +7,26 @@ class MNISTLogReg:
         self.loss = None
         self.logits = None
         self.global_step = None
+        self.reg = tf.contrib.layers.l2_regularizer(scale=0.0001)
 
     def _create_summaries(self):
+        base_summaries = tf.get_collection(tf.GraphKeys.SUMMARIES)
         with tf.name_scope('summaries/train'):
-            tf.summary.scalar('loss', self.loss)
-            tf.summary.histogram('histogram_loss', self.loss)
-            self.train_summary_op = tf.summary.merge_all()
+            train_loss = tf.summary.scalar('loss', self.loss)
+            train_histo_loss = tf.summary.histogram(
+                'histogram_loss', self.loss
+            )
+            train_summaries = [train_loss, train_histo_loss]
+            train_summaries.extend(base_summaries)
+            self.train_summary_op = tf.summary.merge(train_summaries)
         with tf.name_scope('summaries/valid'):
-            tf.summary.scalar('loss', self.loss)
-            tf.summary.histogram('histogram_loss', self.loss)
-            self.valid_summary_op = tf.summary.merge_all()
+            valid_loss = tf.summary.scalar('loss', self.loss)
+            valid_histo_loss = tf.summary.histogram(
+                'histogram_loss', self.loss
+            )
+            valid_summaries = [valid_loss, valid_histo_loss]
+            valid_summaries.extend(base_summaries)
+            self.valid_summary_op = tf.summary.merge(valid_summaries)
 
     def build_network(self, features, targets):
 
@@ -33,26 +43,33 @@ class MNISTLogReg:
                 name='weights',
                 initializer=tf.random_normal(
                     shape=[784, 10], mean=0.0, stddev=0.01, dtype=tf.float32
-                )
+                ),
+                regularizer=self.reg
             )
             self.b = tf.get_variable(
                 name='bias',
                 initializer=tf.random_normal(
                     shape=[10], mean=0.0, stddev=0.01, dtype=tf.float32
-                )
+                ),
+                regularizer=self.reg
             )
             self.logits = tf.add(
                 tf.matmul(self.X, self.W), self.b, name='logits'
             )
 
         with tf.variable_scope('loss'):
+            self.regularization_losses = sum(
+                tf.get_collection(
+                    tf.GraphKeys.REGULARIZATION_LOSSES
+                )
+            )
             self.loss = tf.reduce_mean(
                 tf.nn.softmax_cross_entropy_with_logits(
                     logits=self.logits, labels=self.Y
                 ),
                 axis=0,
                 name='loss'
-            )
+            ) + self.regularization_losses
 
         with tf.variable_scope('training'):
             self.optimizer = tf.train.GradientDescentOptimizer(
