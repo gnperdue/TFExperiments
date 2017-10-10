1e387cd9bb77a1284f707f15de8b418df0087279
Gabriel Perdue <gnperdue@me.com>
2017-10-10 18:01:16 -0500

    clean up whitespace

--- conv/ModelsMNIST.py
+++ conv/ModelsMNIST.py
@@ -319,7 +319,7 @@ class MNISTMLP:
                     biases_name, biases_shape,
                     initializer=tf.random_normal_initializer(
                         mean=0.0, stddev=0.01
-                    )                    
+                    )
                 )
                 # fc_lyr2 = tf.nn.dropout(
                 #     fc_lyr2, self.dropout_keep_prob, name='relu_dropout'
