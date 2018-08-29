679cf852980aaedfe184d6de317ea72c42fdd957
Gabriel Perdue <gnperdue@me.com>
2018-08-29 15:22:49 -0500

    train and test methods

--- fashion/eager_fashion.py
+++ fashion/eager_fashion.py
@@ -7,7 +7,7 @@ import os
 import tensorflow as tf
 
 from tffashion.model_classes import ShallowFashionModel
-from tffashion.data_readers import make_fashion_iterators
+from tffashion.data_readers import make_fashion_dset
 
 tfe = tf.contrib.eager
 tf.logging.set_verbosity(tf.logging.DEBUG)
@@ -42,19 +42,81 @@ def loss(model, x, y):
 def grad(model, inputs, targets):
     with tf.GradientTape() as tape:
         loss_value = loss(model, inputs, targets)
-    return tape.gradient(loss_value, model.variables)
+    return loss_value, tape.gradient(loss_value, model.variables)
+
+
+def train(
+    model, optimizer, dataset, global_step, checkpoint, checkpoint_prefix
+):
+    # must include a record_summaries_method
+    with tf.contrib.summary.record_summaries_every_n_global_steps(20):
+        for (i, (images, labels)) in enumerate(dataset):
+            global_step.assign_add(1)
+            train_loss, grads = grad(model, images, labels)
+            optimizer.apply_gradients(
+                zip(grads, model.variables), global_step=global_step
+            )
+            tf.contrib.summary.scalar('loss', train_loss)
+            if i % 20 == 0:
+                print('loss at step {:03d}: {:.3f}'.format(i, train_loss))
+                checkpoint.save(file_prefix=checkpoint_prefix)
+            if i > 20:
+                break
+        checkpoint.save(file_prefix=checkpoint_prefix)
+
+
+def test(model, dataset):
+    avg_loss = tfe.metrics.Mean('loss', dtype=tf.float32)
+    accuracy = tfe.metrics.Accuracy('accuracy', dtype=tf.float32)
+
+    for (images, labels) in dataset:
+        logits = model(images)
+        avg_loss(loss(model, images, labels))
+        accuracy(
+            tf.argmax(logits, axis=1, output_type=tf.int32),
+            tf.argmax(labels, axis=1, output_type=tf.int32)
+        )
+
+    print('Test set: Average loss: %.4f, Accuracy: %4f%%\n' %
+          (avg_loss.result(), 100 * accuracy.result()))
+    # need a separate writer (either `with`or as default) to keep distinct
+    # with tf.contrib.summary.always_record_summaries():
+    #     tf.contrib.summary.scalar('loss', avg_loss.result())
+    #     tf.contrib.summary.scalar('accuracy', accuracy.result())
 
 
 def main(batch_size, train_steps):
     tf.enable_eager_execution()
     model = ShallowFashionModel()
-    train_img, train_lab = make_fashion_iterators(
-        TRAINFILE, batch_size, num_epochs=1
+    dataset = make_fashion_dset(TRAINFILE, batch_size, num_epochs=1)
+
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+
+    model_dir = '/tmp/fashion'
+    # writer _can_ make its own log directory
+    writer = tf.contrib.summary.create_file_writer(model_dir)
+    global_step = tf.train.get_or_create_global_step()
+    # writer.set_as_default()  # use a scope instead...
+
+    # os.makedirs(model_dir)   # model dir must exist
+    checkpoint_prefix = os.path.join(model_dir, 'ckpt')
+    checkpoint = tfe.Checkpoint(
+        optimizer=optimizer, model=model, optimizer_step=global_step
     )
+    checkpoint.restore(tf.train.latest_checkpoint(model_dir))
 
-    # optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    x, y = iter(dataset).next()
+    print('initial loss: {:.3f}'.format(loss(model, x, y)))
 
-    print('initial loss: {:.3f}'.format(loss(model, train_img, train_lab)))
+    # training loop
+    with writer.as_default():
+        train(
+            model, optimizer, dataset, global_step,
+            checkpoint, checkpoint_prefix
+        )
+
+    test_dataset = make_fashion_dset(TESTFILE, batch_size, num_epochs=1)
+    test(model, test_dataset)
 
 
 if __name__ == '__main__':
