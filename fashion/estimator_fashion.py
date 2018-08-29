d4951860a235c16d93ed2979791abc94d7631ed3
Gabriel Perdue <gnperdue@me.com>
2018-08-29 17:32:41 -0500

    update estimator main script to support tfrecord option, arbitrary file paths for data

--- fashion/estimator_fashion.py
+++ fashion/estimator_fashion.py
@@ -3,11 +3,10 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
-import os
 import tensorflow as tf
 
-# from tffashion.estimator_fns import make_shallow_keras_estimator
 from tffashion.data_readers import make_fashion_iterators
+from tffashion.data_readers import get_data_files_dict
 from tffashion.estimator_fns import shallow_model_fn
 
 
@@ -15,22 +14,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 parser.add_argument('--train_steps', default=None, type=int,
                     help='number of training steps')
-parser.add_argument('--n_epochs', default=1, type=int, help='number of epochs')
-
-# Get path to data
-TESTFILE = os.path.join(
-    os.environ['HOME'], 'Dropbox/Data/RandomData/hdf5/fashion_test.hdf5'
-)
-TRAINFILE = os.path.join(
-    os.environ['HOME'], 'Dropbox/Data/RandomData/hdf5/fashion_train.hdf5'
-)
+parser.add_argument('--num_epochs', default=1, type=int,
+                    help='number of epochs')
+parser.add_argument('--data_dir', default='', type=str, help='data dir')
+parser.add_argument('--tfrecord', default=False, action='store_true',
+                    help='use tfrecords')
+parser.add_argument('--model_dir', default='fashion', type=str,
+                    help='model dir')
 
 
-def predict(classifier, batch_size):
+def predict(classifier, data_files, hyper_pars):
     # predictions is a generator - evaluation is lazy
     predictions = classifier.predict(
         input_fn=lambda: make_fashion_iterators(
-            TESTFILE, batch_size, num_epochs=1
+            data_files['test'], hyper_pars['batch_size'],
+            tfrecord=hyper_pars['tfrecord']
         ),
     )
     counter = 0
@@ -41,10 +39,11 @@ def predict(classifier, batch_size):
             break
 
 
-def evaluate(classifier, batch_size):
+def evaluate(classifier, data_files, hyper_pars):
     eval_result = classifier.evaluate(
         input_fn=lambda: make_fashion_iterators(
-            TESTFILE, batch_size, num_epochs=1
+            data_files['test'], hyper_pars['batch_size'],
+            tfrecord=hyper_pars['tfrecord']
         ),
         steps=100,
     )
@@ -54,30 +53,40 @@ def evaluate(classifier, batch_size):
     ))
 
 
-def train_one_epoch(classifier, batch_size, train_steps):
+def train_one_epoch(classifier, data_files, hyper_pars):
     classifier.train(
         input_fn=lambda: make_fashion_iterators(
-            TRAINFILE, batch_size, num_epochs=1
+            data_files['train'], hyper_pars['batch_size'],
+            tfrecord=hyper_pars['tfrecord']
         ),
-        steps=train_steps
+        steps=hyper_pars['train_steps']
     )
 
 
-def train(classifier, batch_size, num_epochs, train_steps):
-    for i in range(num_epochs):
+def train(classifier, data_files, hyper_pars):
+    for i in range(hyper_pars['num_epochs']):
         print('training epoch {}'.format(i))
-        train_one_epoch(classifier, batch_size, train_steps)
+        train_one_epoch(classifier, data_files, hyper_pars)
         print('evaluation after epoch {}'.format(i))
-        evaluate(classifier, batch_size)
+        evaluate(classifier, data_files, hyper_pars)
 
 
-def main(batch_size, train_steps, n_epochs):
+def main(
+    batch_size, train_steps, num_epochs, data_dir, tfrecord, model_dir
+):
+
+    data_files = get_data_files_dict(path=data_dir, tfrecord=tfrecord)
+    hyper_pars = {}
+    hyper_pars['batch_size'] = batch_size
+    hyper_pars['num_epochs'] = num_epochs
+    hyper_pars['train_steps'] = train_steps
+    hyper_pars['tfrecord'] = tfrecord
 
     run_config = tf.estimator.RunConfig(
         save_checkpoints_steps=10,
         save_summary_steps=10,
         keep_checkpoint_max=3,
-        model_dir='/tmp/fashion',
+        model_dir=model_dir,
         tf_random_seed=None,
     )
     classifier = tf.estimator.Estimator(
@@ -85,8 +94,8 @@ def main(batch_size, train_steps, n_epochs):
         params={},
         config=run_config
     )
-    train(classifier, batch_size, n_epochs, train_steps)
-    predict(classifier, batch_size)
+    train(classifier, data_files, hyper_pars)
+    predict(classifier, data_files, hyper_pars)
 
 
 if __name__ == '__main__':
