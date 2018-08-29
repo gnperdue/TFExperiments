29e12c216a2f5ad1ab10c08c8960c92df7fbdcd6
Gabriel Perdue <gnperdue@me.com>
2018-08-29 17:31:56 -0500

    modify data readers to include a TFRecord option

--- fashion/tffashion/data_readers.py
+++ fashion/tffashion/data_readers.py
@@ -2,20 +2,17 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+import os
 import tensorflow as tf
 
 
-def _make_fashion_generator_fn(hdf5_file_name, use_oned_data=False):
+def _make_fashion_generator_fn(file_name):
     """
     make a generator function that we can query for batches
     """
     from tffashion.hdf5_readers import FashionHDF5Reader as HDF5Reader
-    reader = HDF5Reader(hdf5_file_name)
+    reader = HDF5Reader(file_name)
     nevents = reader.openf()
-    if use_oned_data:
-        readfn = reader.get_flat_example
-    else:
-        readfn = reader.get_example
 
     def example_generator_fn():
         idx = 0
@@ -23,52 +20,80 @@ def _make_fashion_generator_fn(hdf5_file_name, use_oned_data=False):
             if idx >= nevents:
                 reader.closef()
                 return
-            yield readfn(idx)
+            yield reader.get_example(idx)
             idx += 1
 
     return example_generator_fn
 
 
-def make_fashion_dset(hdf5_file, batch_size, num_epochs, use_oned_data=False):
-    # make a generator function
-    dgen = _make_fashion_generator_fn(hdf5_file, use_oned_data)
-
-    # make a Dataset from a generator
-    if use_oned_data:
-        features_shape = [784]
-    else:
-        features_shape = [28, 28, 1]
-    labels_shape = [10]
-    ds = tf.data.Dataset.from_generator(
-        dgen, (tf.float32, tf.uint8),
-        (tf.TensorShape(features_shape), tf.TensorShape(labels_shape))
+def _parse_mnist_tfrec(tfrecord):
+    tfrecord_features = tf.parse_single_example(
+        tfrecord,
+        features={
+            'images': tf.FixedLenFeature([], tf.string),
+            'labels': tf.FixedLenFeature([], tf.string)
+        },
+        name='data'
     )
-    ds = ds.shuffle(1000).batch(batch_size).repeat(num_epochs)
+    images = tf.decode_raw(tfrecord_features['images'], tf.uint8)
+    # note, 'NCHW' is only supported on GPUs, so use 'NHWC'...
+    images = tf.reshape(images, [-1, 28, 28, 1])
+    images = tf.cast(images, tf.float32)
+    labels = tf.decode_raw(tfrecord_features['labels'], tf.uint8)
+    labels = tf.one_hot(indices=labels, depth=10, on_value=1, off_value=0)
+    labels = tf.reshape(labels, [10])
+    return images, labels
+
+
+def make_fashion_dset(
+    file_name, batch_size, num_epochs=1, tfrecord=False
+):
+    if tfrecord:
+        ds = tf.data.TFRecordDataset([file_name], compression_type='GZIP')
+        # if shuffle:
+        #     ds = ds.shuffle(buffer_size=256)
+        ds = ds.map(_parse_mnist_tfrec).prefetch(batch_size)
+        ds = ds.batch(batch_size).repeat(num_epochs)
+    else:
+        # make a generator function - read from HDF5
+        dgen = _make_fashion_generator_fn(file_name)
+
+        # make a Dataset from a generator
+        features_shape = [28, 28, 1]
+        labels_shape = [10]
+        ds = tf.data.Dataset.from_generator(
+            dgen, (tf.float32, tf.uint8),
+            (tf.TensorShape(features_shape), tf.TensorShape(labels_shape))
+        )
+        ds = ds.prefetch(10*batch_size)
+        ds = ds.shuffle(10*batch_size).batch(batch_size).repeat(num_epochs)
+
     return ds
 
 
 def make_fashion_iterators(
-        hdf5_file, batch_size, num_epochs, use_oned_data=False
+        file_name, batch_size, num_epochs=1, tfrecord=False
 ):
     '''
     estimators require an input fn returning `(features, labels)` pairs, where
     `features` is a dictionary of features.
+
+    TODO - pass a shuffle flag
     '''
-    ds = make_fashion_dset(hdf5_file, batch_size, num_epochs, use_oned_data)
+    ds = make_fashion_dset(file_name, batch_size, num_epochs, tfrecord)
 
     # one_shot_iterators do not have initializers
     itrtr = ds.make_one_shot_iterator()
     feats, labs = itrtr.get_next()
     return feats, labs
-    # return ds
 
 
-def make_train_input_function(shuffle=True, shuffle_depth=1000):
-    '''
-    estimators require an input fn with a specific signature - customize that
-    function here.
-    '''
-    def train_input_fn(features, labels, batch_size):
-        pass
-
-    return train_input_fn
+def get_data_files_dict(path='path_to_data', tfrecord=False):
+    data_dict = {}
+    if tfrecord:
+        data_dict['train'] = os.path.join(path, 'fashion_train.tfrecord.gz')
+        data_dict['test'] = os.path.join(path, 'fashion_test.tfrecord.gz')
+    else:
+        data_dict['train'] = os.path.join(path, 'fashion_train.hdf5')
+        data_dict['test'] = os.path.join(path, 'fashion_test.hdf5')
+    return data_dict
