fc75db0cef9ac56a167f5afe947e370ff6feec0b
Gabriel Perdue <gnperdue@me.com>
2018-08-30 09:49:07 -0500

    misc futzing around

--- fashion/run_fashion_estimator.sh
+++ fashion/run_fashion_estimator.sh
@@ -6,13 +6,14 @@ NUM_EPOCHS=2
 MODELDIR="/tmp/fashion_estimator"
 
 
-DATA_DIR="/Users/perdue/Dropbox/Data/RandomData/TensorFlow"
-TFRECORD="--tfrecord"
-
 DATA_DIR="/Users/perdue/Dropbox/Data/RandomData/hdf5"
 TFRECORD=""
 
+DATA_DIR="/Users/perdue/Dropbox/Data/RandomData/TensorFlow"
+TFRECORD="--tfrecord"
 
-ARGS="--batch_size ${BATCH_SIZE} --train_steps ${TRAIN_STEPS} --num_epochs ${NUM_EPOCHS} --data_dir ${DATA_DIR} $TFRECORD --model_dir $MODELDIR"
+
+ARGS="--batch_size ${BATCH_SIZE} --train_steps ${TRAIN_STEPS} --num_epochs
+${NUM_EPOCHS} --data_dir ${DATA_DIR} $TFRECORD --model_dir $MODELDIR"
 
 python $EXE $ARGS
