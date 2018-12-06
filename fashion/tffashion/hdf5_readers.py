f7cdd0a270213c805a77a62f22fd42718722dbe9
Gabriel Perdue <gnperdue@gmail.com>
2018-12-06 17:11:02 -0600

    add a `tofloat` option to the constructor and more ways to get flat examples back

--- fashion/tffashion/hdf5_readers.py
+++ fashion/tffashion/hdf5_readers.py
@@ -13,14 +13,15 @@ class FashionHDF5Reader(object):
     assumes stored image shape is [N, depth(=1 greyscale), H, W]
     """
 
-    def __init__(self, hdf5_file):
+    def __init__(self, hdf5_file, tofloat=False):
         self._file = hdf5_file
         self._f = None
-        self._nlabels = 10
         self.class_names = [
             'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
             'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
         ]
+        self._nlabels = len(self.class_names)
+        self._tofloat = tofloat
 
     def openf(self):
         self._f = h5py.File(self._file, 'r')
@@ -39,6 +40,9 @@ class FashionHDF5Reader(object):
         label = self._f['fashion/labels'][idx].reshape([-1])
         oh_label = np.zeros((1, self._nlabels), dtype=np.uint8)
         oh_label[0, label] = 1
+        if self._tofloat:
+            return image.astype(np.float32), \
+                oh_label.reshape(self._nlabels,).astype(np.float32)
         return image, oh_label.reshape(self._nlabels,)
 
     def get_flat_example(self, idx):
@@ -52,4 +56,11 @@ class FashionHDF5Reader(object):
         label = self._f['fashion/labels'][start_idx: stop_idx].reshape([-1])
         oh_label = np.zeros((label.size, self._nlabels), dtype=np.uint8)
         oh_label[np.arange(label.size), label] = 1
+        if self._tofloat:
+            return image.astype(np.float32), oh_label.astype(np.float32)
         return image, oh_label
+
+    def get_flat_examples(self, start_idx, stop_idx):
+        image, label = self.get_examples(start_idx, stop_idx)
+        image = np.reshape(image, (-1, 28 * 28))
+        return image, label
