acee06659e87889cfd382403541f9a7c9c54ae38
Gabriel Perdue <gnperdue@me.com>
2018-12-09 06:10:04 -0600

    add an 'in_memory' option for loading an entire HDF5 (but not TFRecord at this point) into memory and making a `Dataset` out of that.

--- fashion/tffashion/data_readers.py
+++ fashion/tffashion/data_readers.py
@@ -3,7 +3,19 @@ from __future__ import absolute_import
 from __future__ import division
 
 import os
+import numpy as np
 import tensorflow as tf
+
+
+def _make_numpy_data_from_hdf5(file_name):
+    from tffashion.hdf5_readers import FashionHDF5Reader as HDF5Reader
+    reader = HDF5Reader(file_name)
+    nevents = reader.openf(make_data_dict=True)
+    features = reader.data_dict['images']
+    features = np.moveaxis(features, 1, -1)
+    targets = reader.data_dict['labels']
+    reader.closef()
+    return nevents, features, targets
 
 
 def _make_fashion_generator_fn(file_name, batch_size):
@@ -46,7 +58,8 @@ def _parse_mnist_tfrec(tfrecord):
 
 
 def make_fashion_dset(
-    file_name, batch_size, num_epochs=1, shuffle=False, tfrecord=False
+    file_name, batch_size, num_epochs=1, shuffle=False, tfrecord=False,
+    in_memory=True
 ):
     if tfrecord:
         ds = tf.data.TFRecordDataset([file_name], compression_type='GZIP')
@@ -55,28 +68,38 @@ def make_fashion_dset(
         if shuffle:
             ds = ds.shuffle(buffer_size=10*batch_size)
     else:
-        # make a generator function - read from HDF5
-        dgen = _make_fashion_generator_fn(file_name, batch_size)
+        if in_memory:
+            _, features, targets = _make_numpy_data_from_hdf5(file_name)
+            ds = tf.data.Dataset.from_tensor_slices((
+                features.astype(np.float32), targets
+            ))
+            if shuffle:
+                ds = ds.shuffle(10000)
+            ds = ds.repeat(num_epochs)
+            ds = ds.batch(batch_size)
+        else:
+            # make a generator function - read from HDF5
+            dgen = _make_fashion_generator_fn(file_name, batch_size)
 
-        # make a Dataset from a generator
-        features_shape = [None, 28, 28, 1]
-        labels_shape = [None, 10]
-        ds = tf.data.Dataset.from_generator(
-            dgen, (tf.float32, tf.uint8),
-            (tf.TensorShape(features_shape), tf.TensorShape(labels_shape))
-        )
-        # we are grabbing an entire "batch", so don't call `batch()`, etc.
-        # also, note, there are issues with doing more than one epoch for
-        # `from_generator` - so do just one epoch at a time for now.
-        ds = ds.prefetch(10)
-        if shuffle:
-            ds = ds.shuffle(10)
+            # make a Dataset from a generator
+            features_shape = [None, 28, 28, 1]
+            labels_shape = [None, 10]
+            ds = tf.data.Dataset.from_generator(
+                dgen, (tf.float32, tf.uint8),
+                (tf.TensorShape(features_shape), tf.TensorShape(labels_shape))
+            )
+            # we are grabbing an entire "batch", so don't call `batch()`, etc.
+            # also, note, there are issues with doing more than one epoch for
+            # `from_generator` - so do just one epoch at a time for now.
+            ds = ds.prefetch(10)
+            if shuffle:
+                ds = ds.shuffle(10)
 
     return ds
 
 
 def make_fashion_iterators(
-        file_name, batch_size, num_epochs=1, shuffle=False, tfrecord=False
+    file_name, batch_size, num_epochs=1, shuffle=False, tfrecord=False
 ):
     '''
     estimators require an input fn returning `(features, labels)` pairs, where
